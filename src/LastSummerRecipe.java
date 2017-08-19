260452e9782f134bd60bfef127fbd1ae76d23db5
joshpryor <league@Admins-Mac-mini-2.local>
2017-08-19 11:23:34 -0700

    u wot m8

---
+++ src/LastSummerRecipe.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class LastSummerRecipe {
+	public static void main(String[] args) {
+		
+		String name =JOptionPane.showInputDialog("Whats your name?");
+		
+		JOptionPane.showMessageDialog(null, "I know that you played last summer "+name+"!");
+		
+		
+		
+	}
+}	
+//4. Use JOptionPane.showMessageDialog to tell them you know what they did last summer,
+//e.g.  I know that you [activity] last summer, [name]. Muhahaha!
+
